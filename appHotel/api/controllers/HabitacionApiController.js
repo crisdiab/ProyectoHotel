0cecaaab8bfcd47d4f6887c04335e82f44d58c81
Armando115Tapia <jusnfds123@hotmail.com>
2017-01-19 12:43:08 -0500

    Creacion Habitacion

---
+++ appHotel/api/controllers/HabitacionApiController.js
@@ -0,0 +1,11 @@
+/**
+ * HabitacionApiController
+ *
+ * @description :: Server-side logic for managing Habitacionapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
