b4a0843b5c38f39821007c54a81e183f7ddc8b65
crisdiab <cristian89lara@gmail.com>
2017-01-19 12:49:19 -0500

    modifcado atributos usuario

--- appHotel/api/models/UsuarioApi.js
+++ appHotel/api/models/UsuarioApi.js
@@ -8,7 +8,53 @@
 module.exports = {
 
   attributes: {
-    id
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      unique: true,
+      columnName: 'id'
+    },
+    nombre: {
+      type: 'string',
+      columnName: 'nombre',
+      required: true
+
+    },
+    apellido: {
+      type: 'string',
+      columnName: 'apellido',
+      required: true
+
+    },
+    cedula: {
+      type: 'string',
+      columnName: 'cedula',
+      unique: true,
+      required: true
+
+    },
+    facturas: {
+      collection: 'facturaapi',
+      via: 'idComprador'
+    },
+    usuario: {
+      type: 'string',
+      unique: true,
+      defaultsTo:'',
+
+    },
+    password: {
+      type: "string",
+      unique: true,
+
+    },
+    nivelAcceso: {
+      type: "integer",
+      defaultsTo:1,
+
+
+    },
+
   }
 };
 
