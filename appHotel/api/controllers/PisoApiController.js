01d71755178abfd375684df6f99d3ce667a7b4c4
sanierjd <sanierjd@live.com>
2017-01-19 12:45:24 -0500

    Creación Api PisoApi

---
+++ appHotel/api/controllers/PisoApiController.js
@@ -0,0 +1,11 @@
+/**
+ * PisoApiController
+ *
+ * @description :: Server-side logic for managing Pisoapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
