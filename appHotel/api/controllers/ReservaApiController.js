03b17318e2c10aa7f47dcc6f88736489fe76a14c
wilgue10 <wilmer.guevara@outlook.es>
2017-01-22 20:42:23 -0500

    creacionModuloReserva

---
+++ appHotel/api/controllers/ReservaApiController.js
@@ -0,0 +1,11 @@
+/**
+ * ReservaApiController
+ *
+ * @description :: Server-side logic for managing reservaapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
