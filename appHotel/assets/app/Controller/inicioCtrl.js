890af804601e72222263681bd3cb7acdab4d4b2d
crisdiab <cristian89lara@gmail.com>
2017-01-28 09:37:15 -0500

    incio creado

---
+++ appHotel/assets/app/Controller/inicioCtrl.js
@@ -0,0 +1,16 @@
+aplicacion.controller('inicioCtrl', [
+  '$scope',
+
+  '$http',
+  '$cookies',
+  function ($scope, $http,$cookies) {
+
+    // $scope.logout = function(){
+    //
+    //   $cookies.remove('UsuarioId');
+    //   $cookies.remove('UsuarioTipo');
+    //   console.log('hizo logout')
+    //
+    // }
+  }]);
+
